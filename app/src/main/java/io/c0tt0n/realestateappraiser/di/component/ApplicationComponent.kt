6454f7527cdba8bb420a25fe13d449081a9cd4fc
yokubo <utsue.38@gmail.com>
2019-03-06 22:59:02 +0900

    rename and move binding module for Activity

--- app/src/main/java/io/c0tt0n/realestateappraiser/di/component/ApplicationComponent.kt
+++ app/src/main/java/io/c0tt0n/realestateappraiser/di/component/ApplicationComponent.kt
@@ -4,12 +4,12 @@ import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
 import io.c0tt0n.realestateappraiser.AppraiserApplication
-import io.c0tt0n.realestateappraiser.di.builder.ActivityBuildingModule
+import io.c0tt0n.realestateappraiser.di.module.ActivityBindingModule
 import io.c0tt0n.realestateappraiser.di.module.ApplicationModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [(AndroidSupportInjectionModule::class), (ApplicationModule::class), ActivityBuildingModule::class])
+@Component(modules = [(AndroidSupportInjectionModule::class), (ApplicationModule::class), ActivityBindingModule::class])
 interface ApplicationComponent : AndroidInjector<AppraiserApplication> {
 
     @Component.Builder
