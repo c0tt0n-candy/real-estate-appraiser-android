9a37ef95082213ec6c4deb61f93bd2b869298b3a
yokubo <utsue.38@gmail.com>
2019-03-15 21:43:49 +0900

    add toolbar to Standards screen

--- app/src/main/java/io/c0tt0n/realestateappraiser/ui/standards/StandardsActivity.kt
+++ app/src/main/java/io/c0tt0n/realestateappraiser/ui/standards/StandardsActivity.kt
@@ -3,9 +3,11 @@ package io.c0tt0n.realestateappraiser.ui.standards
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import dagger.android.support.DaggerAppCompatActivity
 import io.c0tt0n.realestateappraiser.R
 import io.c0tt0n.realestateappraiser.util.replaceFragment
+import kotlinx.android.synthetic.main.standards_act.*
 import javax.inject.Inject
 
 class StandardsActivity : DaggerAppCompatActivity() {
@@ -20,10 +22,22 @@ class StandardsActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.standards_act)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         supportFragmentManager.findFragmentById(R.id.fragment_container) as StandardsFragment?
                 ?: run {
                     replaceFragment(R.id.fragment_container, fragment)
                 }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
